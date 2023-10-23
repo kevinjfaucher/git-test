1d8878bc9ffee9edac084b63cc76bcec6141f40a
kevinjfaucher <126988757+kevinjfaucher@users.noreply.github.com>
2023-10-23 18:43:42 -0400

    Update App.js
    breaking the code as a demo

--- App.js
+++ App.js
@@ -1,22 +1,4 @@
-import React from 'react';
-import { StyleSheet, View, FlatList, Text } from 'react-native';
 
-export default class SimpleFlatListApp extends React.Component {
-  render() {
-    // Inline data for the list.
-    // This is a test line!!!!!!!!!!!
-    //  This is a change for my new branch!!!!!!
-    const data = ['Apple', 'Banana', 'Cherry', 'Date', 'Fig', 'Grape', 'Honeydew'];
-
-    return (
-        <View style={styles.container}>
-          <FlatList
-              data={data}
-              // Inline renderItem function to specify how to display each item
-              renderItem={({ item }) => <Text style={styles.itemText}>{item}</Text>}
-              // Providing a unique key for each item
-              keyExtractor={(item) => item}
-          />
         </View>
     );
   }
